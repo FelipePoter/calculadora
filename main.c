9ea5fcdb10ea591e1ecaa2455ccf09f34e4a303a
Felipe de Carvalho Hesse <hesse.lipe26@gmail.com>
2025-11-01 13:53:12 -0300

    Corrigir erros
    Refactor input handling for option selection.

--- main.c
+++ main.c
@@ -18,15 +18,16 @@ int main()
     printf("5. Exit\n");
     printf("Option: ");
 
-    while (scanf("%d", &option) != 1 || option < 1 || option > 5)
-    {
-      while (getchar() != '\n');
-      printf("Entrada invalida. Por favor insira um numero entre 1 e 5.\n");
-      printf("Option: ");
-    }
-
     scanf("%d", &option);
     getchar();
+
+    while (scanf("%d", &option) != 1 || option < 1 || option > 5)
+    {
+      while (getchar() != '\n')
+        ;
+      printf("Entrada invalida. Por favor, insira um numero entre 1 e 5.\n");
+      printf("Option: ");
+    }
 
     if (option == 1)
     {
